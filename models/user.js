ab63609fb4b283163da4719eeadcc724cd73614b
Jarod Reyes <jarodreyes@gmail.com>
2017-04-21 13:59:30 -0700

    Added ability to customize pet search.

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@
 var mongoose = require('mongoose');
 var config = require('../config');
 var twilioClient = require('twilio')(config.accountSid, config.authToken);
-var Emailer = require('../lib/sendgrid-emailer');
+var Emailer = require('../lib/sendgrid-emailer'),
+_ = require('underscore');
 
 var Pet = new mongoose.Schema({
   id: String,
@@ -20,29 +21,103 @@ var User = new mongoose.Schema({
   zipcode: String,
   animalPref: String,
   sizePref: String,
+  agePref: String, default: '',
   sexPref: String,
   catPref: String,
-  lastOffset: Number, default: 0,
-  housetrained: Boolean,
+  editing: Boolean, default: false,
+  lastOffset: {type: Number, default: 0, min: 0, required: true},
+  housePref: String, default: '',
 	state: { type: String, enum: states, default: 'new' },
+  tags: Array,
   pets: [Pet]
 });
 
+// Add tags to the user model, look these up to match pets
+User.methods.addTags = function(tags, callback) {
+  var self = this;
+  _.each(tags, function(tag) {
+    self.tags.push(tag);
+  });
+  self.tags = _.uniq(self.tags);
+  if (_.contains(self.tags, 'indoors')) {
+    self.housePref = 'small';
+  };
+  if (_.contains(self.tags, 'outdoors')) {
+    self.housePref = 'large';
+  };
+  self.save();
+  callback();
+}
+
 // Send a text message via twilio to this user
+User.methods.sendMessage = function(msg, media) {
+  var self = this;
+  var options = {
+    to: self.phone,
+    from: config.twilioNumber,
+    body: msg
+  }
+  if (media) options.mediaUrl = media;
+  twilioClient.sendMessage(options, function(err, response) {
+    console.log(`Twilio Message Sent: ${err}`);
+  });
+};
+
+// Send a pet
 User.methods.sendPet = function(pet) {
-  var truncated = pet.description.length > 1600 ? pet.description.substring(0, 1200) + '...' : pet.description;
+  if (pet.description) {
+    var truncated = pet.description.length > 1600 ? pet.description.substring(0, 1200) + '...' : pet.description;
+  } else {
+    var truncated = 'Description not available from Shelter.'
+  }
   var self = this,
     media = pet.media.photos['1'].x,
     message = `${pet.name}: ${truncated}. Contact Info: ${pet.contact.email}, ${pet.contact.phone}.`;
-  twilioClient.sendMessage({
-    to: self.phone,
-    from: config.twilioNumber,
-    mediaUrl: media,
-    body: message
-  }, function(err, response) {
-    console.log(err);
-  });
+  self.sendMessage(message, media);
 };
+
+// set property to null
+User.methods.editingPrefs = function(body, callback) {
+  var self = this;
+  self[`${body}Pref`] = null;
+  self.editing = true;
+  self.save();
+  callback();
+};
+
+// set property 
+User.methods.setPref = function(pref, setting) {
+  var self = this;
+  self[`${pref}`] = setting;
+  console.log(pref, setting);
+  self.editing = false;
+  self.save();
+  return `Great we have updated your preferences to find a ${setting} pet.`;
+};
+
+// Send a text message via twilio to this user
+User.methods.advanceStatus = function() {
+  var self = this;
+  if (self.state == 'new') {
+    self.state = 'customizing';
+  } else {
+    self.state = 'pairing';
+  }
+  self.save();
+};
+
+// Send a text message via twilio to this user
+User.methods.regressStatus = function() {
+  var self = this;
+  if (self.state == 'pairing') {
+    self.state = 'customizing';
+  } else {
+    self.state = 'new';
+  }
+  self.save();
+};
+
+
 
 // Send a text message via twilio to this user
 User.methods.sendEmail = function() {
--- routes/shakespeare.js
+++ routes/shakespeare.js
@@ -4,6 +4,7 @@ var express = require('express'),
   router = express.Router(),
   twilio = require('twilio'),
   petMatcher = require('../lib/petmatcher'),
+  ImageChecker = require('../lib/image-checker'),
   User = require('../models/user'),
   _ = require('underscore');
 
@@ -23,13 +24,33 @@ function twimlMessage (message, media) {
   return output;
 }
 
-var animals = ['dog', 'cat', 'bird', 'other'],
-  sizes = ['small', 'medium', 'large', 'xl', 'any'],
-  sex = ['m', 'f'];
+function printTags (tags) {
+  var uMessage = "Okay I've analyzed your picture, I detect ";
+  var smallTags = _.sample(tags, 4);
+  console.log(`in printTags: ${smallTags}`)
+  _.times(3, function(i) {
+    uMessage += `${smallTags[i]}, `;
+  });
+  uMessage += `and ${smallTags[3]}. This will certainly help find the perfect pet.`;
+  return uMessage;
+}
+
+var animals = ['dog', 'cat'],
+  sizes = ['small', 'medium', 'large', 'xl'],
+  sex = ['m', 'f'],
+  ages = ['baby', 'young', 'adult', 'senior'], 
+  commands = {
+    'age': 'What age animal do you prefer? [Baby, Young, Adult, Senior]',
+    'sex': 'Do you prefer [male] or [female]?',
+    'size': 'What size animal do you want? [Small, Medium, Large, XL, Any]',
+    'animal': 'What are you looking for? [Dog, Cat]',
+    'house': 'Do you care if it is house trained? [Yes, No]'
+  };
 
 // If we are in the romancing state, let's parse the body
 function customizeAnimal(body, user, req) {
   var media = '',
+    numMedia = req.body.NumMedia,
     message = '';
 
   if (!user.zipcode) {
@@ -38,67 +59,76 @@ function customizeAnimal(body, user, req) {
     if (isValidZip) {
       user.zipcode = body;
       user.save();
-      message = `Alright your zip code is: ${user.zipcode}! Now what animal companion are you looking for? [Dog, Cat, Bird, Other]`
+      message = `Alright your zip code is: ${user.zipcode}! Now ${commands.animal}`;
     }
   } else if (!user.animalPref) {
-    message = 'What animal companion are you looking for? [Dog, Cat, Bird, Other]'
+    message = `${commands.animal}`;
     var answered = _.contains(animals, body);
+    if (user.state == 'pairing') {
+      user.regressStatus();
+    }
     if (answered) {
-      user.animalPref = body;
-      user.save();
-      message = 'Got it! What size animal do you want? [Small, Medium, Large, XL, Any] '
+      user.setPref('animalPref', body);
+      message = user.editing ? user.setPref('animalPref', body) : `Okay, now for the fun part. Take a picture of where your adorable ${user.animalPref} will spend most of its time. Your apartment? The backyard? Wherever you live.`;
+    }
+  } else if (!user.agePref) {
+    message = `${commands.age}`;
+    var answered = _.contains(ages, body);
+    if (answered) {
+      message = user.editing ? user.setPref('agePref', body) : `Great! We'll find you a pet soul mate that will thrive in your living environment.`;
+      user.advanceStatus();
+      // Send them a pet!
+      petMatcher.findPet(user);
     }
   } else if (!user.sizePref) {
-    message = `What size ${user.animalPref} do you want? [Small, Medium, Large, XL, Any]`
+    message = `${commands.size}`;
     var answered = _.contains(sizes, body);
-    if (answered && body != 'xl') {
-      user.sizePref = body[0].toUpperCase();
-      user.save();
-      message = 'Awesome! Do you care if it is house trained? [Yes, No]'
-    } else if (body == 'xl') {
-      user.sizePref = 'XL';
-      user.save();
-    }
-  } else if (!user.housetrained) {
-    message = 'Do you care if it is house trained? [Yes, No]'
-    if (body == 'y' || body == 'yes') {
-      user.housetrained = true;
-      user.state = 'pairing';
-      user.save();
-      var pet = petMatcher.findPet(user);
-      message = "Awesome! I'll send you a match shortly. You can always tell me to 'Find another!' and I'll send you another local pet."
-    } else if (body == 'n' || body == 'no') {
-      user.housetrained = false;
-      user.state = 'pairing';
-      user.save();
-      var pet = petMatcher.findPet(user);
-      message = "Awesome! I'll send you a match shortly. You can always tell me to 'Find another!' and I'll send you another local pet."
+    console.log(`Answered: ${answered} - Editing: ${user.editing}`);
+    if (answered) {
+      var size = body == 'xl' ? 'XL' : body[0].toUpperCase();
+      message = user.setPref('sizePref', size);
     }
   }
+
+  if (numMedia > 0) {
+    var incomingMedia = req.body.MediaUrl0;
+    console.log(incomingMedia);
+    message = `Okay we're processing your picture, but one last question. ${commands.age}`
+    ImageChecker.check(incomingMedia, function(tags) {
+      if (tags.length > 0) {
+        user.addTags(tags, function() {
+          message = printTags(tags);
+          user.sendMessage(message);
+        });
+      } else {
+        message = 'Sorry, we were unable to process that image. Try sending a different one.';
+      }
+    });
+  }
+  console.log(`Message: ${message}`);
   return {media: media, message: message}
 }
 
 // Twilio webhook for Message url at https://www.twilio.com/console/phone-numbers/XXXXXXXXXXXXXXXXXXXXXXXXXSID
 router.post('/incoming/', function (req,res,next) {
   var body = req.body.Body.toLowerCase(),
+    numMedia = req.body.NumMedia,
     userPhone = req.body.From,
     message = '',
     media = '';
   res.type('text/xml');
 
   var query = {phone: userPhone},
-    update = { expire: new Date() },
+    update = { expire: new Date()},
     options = { upsert: true, new: true, setDefaultsOnInsert: true };
 
   User.findOneAndUpdate(query, update, options)
     .then(function (user) {
-      // Check if user is new, or is requesting a sonnet.
+      // Check if user is new, or is customizing their pet.
       switch (user.state) {
         case 'new':
-
-          message = "Hello happy pet hunter, I'm ShelterBuddy your helpful pet finder. I have a few questions. What is your zipcode?";
-          user.state = 'customizing';
-          user.save();
+          user.sendMessage("Hello happy pet hunter, I'm ShelterBuddy your helpful pet finder. Tell me about yourself? What is your zipcode?", 'http://jardiohead.s3.amazonaws.com/instructions.jpg')
+          user.advanceStatus();
           break;
         case 'customizing':
           var appropriateResponse = customizeAnimal(body, user, req);
@@ -106,7 +136,19 @@ router.post('/incoming/', function (req,res,next) {
           media = appropriateResponse.media;
           break;
         default:
-          var pet = petMatcher.findPet(user);
+          var customizing = _.has(commands, body);
+          console.log(body, customizing);
+          if (customizing || user.editing) {
+            var appropriateResponse = {};
+            user.editingPrefs(body, function() {
+              appropriateResponse = customizeAnimal(body, user, req);
+            });
+            message = appropriateResponse.message;
+            media = appropriateResponse.media;
+          } else {
+            var pet = petMatcher.findPet(user);
+          }
+          
       }
       var twiml = twimlMessage(message, media);
       res.send(twiml.toString());

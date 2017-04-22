dd0f68214b23ffc723e8ddd0b13f69eb7fa829d3
Jarod Reyes <jarodreyes@gmail.com>
2017-04-21 21:46:11 -0700

    Remove cookie-parser.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
@@ -19,7 +18,6 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
